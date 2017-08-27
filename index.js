32557972dca5c54b5752cc4082e7463ac3e2e3e4
writes <jonathonhthompson@gmail.com>
2017-08-27 17:28:42 -0500

    feat(index): include oauth API info and pass in to index

--- index.js
+++ index.js
@@ -2,12 +2,21 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
 // Create GoogleStrategy instance
 // passport.use as generic register
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+  }, accessToken => {
+    console.log(accessToken);
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
