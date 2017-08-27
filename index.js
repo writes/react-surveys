2c0c817ab92b7591be6a0f7d3351504e62516e59
writes <jonathonhthompson@gmail.com>
2017-08-27 17:04:48 -0500

    refactor(index): create strategy instance and use

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Include express and create app
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-//
-app.get('/', (req, res) => {
-  res.send({hi: 'there' });
-});
+// Create GoogleStrategy instance
+// passport.use as generic register
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
