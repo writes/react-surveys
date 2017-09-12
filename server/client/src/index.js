9e6558c07938d0931f03d60a4b6e226f05fac90f
writes <jonathonhthompson@gmail.com>
2017-09-12 01:48:44 -0500

    feat(reducers): add reducers directory

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -5,12 +5,13 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
- <Provider store={store}><App /></Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 );
-
-
