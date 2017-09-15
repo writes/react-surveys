c81aec4efd3cc11e8dcf5f00cc4980002f39d594
writes <jonathonhthompson@gmail.com>
2017-09-15 00:36:34 -0500

    feat(components): add header

---
+++ server/client/src/components/Header.js
@@ -0,0 +1,11 @@
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <h1>HEADER</h1>
+        </div>
+      </nav>
+    )
+  }
+}
