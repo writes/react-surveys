86133baceb7a51bd91ccf1b6d964530bb0620b1c
writes <jonathonhthompson@gmail.com>
2018-01-27 00:01:35 -0600

    feat(components): add landing component file

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="row">
+        <h2>Landing</h2>
+      </div>
+    )
+  }
+}
+
+export default Landing;
