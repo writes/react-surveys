8e020bcd0cc21bf2c676b19ab4d7265fa6f6248b
writes <jonathonhthompson@gmail.com>
2018-01-27 22:32:37 -0600

    update

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -4,7 +4,8 @@ import { FETCH_USER } from './types';
 export const fetchUser = () => {
     // dispatch action once request is completed
     return function(dispatch) {
-        axios.get('/api/current_user')
+        axios
+        .get('/api/current_user')
         // wait for response from api and then dipatch action
         .then(res => dispatch({ type: FETCH_USER, payload: res }));
     };
--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route d} from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
